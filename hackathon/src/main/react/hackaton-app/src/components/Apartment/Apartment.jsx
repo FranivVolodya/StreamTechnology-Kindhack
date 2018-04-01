1d68a57e98295012df922d08269426ed90ee38df
nazzarkevich <nazzik.3@gmail.com>
2018-04-01 14:06:16 +0300

    add checkbox filter menu

--- hackathon/src/main/react/hackaton-app/src/components/Apartment/Apartment.jsx
+++ hackathon/src/main/react/hackaton-app/src/components/Apartment/Apartment.jsx
@@ -3,6 +3,7 @@ import ApartmentBody from './ApartmentBody';
 import './Apartment';
 import apartImage from '../../asserts/images/flat1.png';
 import './Apartment.scss';
+import { Button, DropdownButton, MenuItem, ButtonToolbar, Checkbox } from 'react-bootstrap';
 import Footer from '../Footer/Footer';
 
 class Apartment extends Component {
@@ -30,6 +31,42 @@ class Apartment extends Component {
                 </h1>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="filter-menu">
+            <ul className="filter-list">
+              <li><ButtonToolbar>
+                <DropdownButton title="Район" id="dropdown-size-medium">
+                {Object.keys(data).map(key => {
+                  return(
+                    <Checkbox
+                      bsClass="claseess"
+                      onChange={e => this.handleChange(e)}
+                      >{data[key].region}
+                      </Checkbox>
+                    )
+                  })}
+                </DropdownButton>
+              </ButtonToolbar></li>
+              <li><ButtonToolbar>
+                <DropdownButton title="Район" id="dropdown-size-medium">
+                  <Checkbox
+                    bsClass="claseess"
+                    onChange={e => this.handleChange(e)}
+                  >Франківський</Checkbox>
+                  <Checkbox
+                    bsClass="claseess"
+                    onChange={e => this.handleChange(e)}
+                  >Галицький</Checkbox>
+                </DropdownButton>
+              </ButtonToolbar></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </div>
         </div>
       </div>
